4e65eb26821b88b8965398c73b0c4cd53e6f6bd6
katttern <katuskatus07@gmail.com>
2026-03-25 14:57:12 +0200

    Implement function to swap node pairs in linked list

---
+++ Swap Node Pairs In Linked List/solution.py
@@ -0,0 +1,13 @@
+from preloaded import Node
+
+def swap_pairs(head):
+    if head is None or head.next is None:
+        return head
+
+    first = head
+    second = head.next
+
+    first.next = swap_pairs(second.next)
+    second.next = first
+
+    return second
